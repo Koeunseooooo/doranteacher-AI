7a2a875a9288b19160e4166a3180aedfa649486c
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-05-22 10:45:28 +0900

    Feat: rq-vae-transformer을 submodule로 받아오기

--- paint.py
+++ paint.py
@@ -1,3 +1,4 @@
+from notebook_utils import TextEncoder, load_model, get_generated_images_by_texts
 import numpy as np
 import itertools
 import requests
@@ -9,13 +10,17 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import transformers
 import yaml
 import torch
 import torchvision
 import clip
 import torch.nn.functional as F
+import time
 
-# from notebooks.notebook_utils import TextEncoder, load_model, get_generated_images_by_texts
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 
 JVM_PATH_TEM = '/Library/Java/JavaVirtualMachines/zulu-15.jdk/Contents/Home/bin/java'
 
@@ -23,6 +28,7 @@ doc = "내가 제일 좋아하는 음식은 햄버거이다. 그래서 오늘은
 
 
 def paint():
+    start = time.time()
     okt = Okt(jvmpath=JVM_PATH_TEM)
     tokenized_doc = okt.pos(doc)
     tokenized_nouns = ' '.join([word[0]
@@ -58,7 +64,63 @@ def paint():
     print(trans)
     input_template = "a Picture containing a "+trans
 
-    return "hello"
+    vqvae_path = '../model/cc3m/stage1/model.pt'
+    model_path = '../model/cc3m/stage2/model.pt'
+
+    # load stage 1 model: RQ-VAE
+    model_vqvae, _ = load_model(vqvae_path)
+
+    # load stage 2 model: RQ-Transformer
+    model_ar, config = load_model(model_path, ema=False)
+
+    # GPU
+    model_ar = model_ar.eval()
+    print("완료1")
+    model_vqvae = model_vqvae.eval()
+    print("완료2")
+
+    # CLIP
+    model_clip, preprocess_clip = clip.load("ViT-B/32", device='cpu')
+    print("완료3")
+    model_clip = model_clip.eval()
+    print("완료4")
+
+    # prepare text encoder to tokenize natual languages
+    text_encoder = TextEncoder(tokenizer_name=config.dataset.txt_tok_name,
+                               context_length=config.dataset.context_length)
+
+    text_prompts = input_template  # your own text
+    num_samples = 64
+    temperature = 1.0
+    top_k = 1024
+    top_p = 0.95
+
+    print("완료5")
+    pixels = get_generated_images_by_texts(model_ar,
+                                           model_vqvae,
+                                           text_encoder,
+                                           model_clip,
+                                           preprocess_clip,
+                                           text_prompts,
+                                           num_samples,
+                                           temperature,
+                                           top_k,
+                                           top_p,
+                                           )
+
+    print("완료6")
+    num_visualize_samples = 12
+    images = [pixel.cpu().numpy() * 0.5 + 0.5 for pixel in pixels]
+    images = torch.from_numpy(np.array(images[:num_visualize_samples]))
+    images = torch.clamp(images, 0, 1)
+    grid = torchvision.utils.make_grid(images, nrow=4)
+
+    img = Image.fromarray(np.uint8(grid.numpy().transpose([1, 2, 0])*255))
+    # img.show()
+    img.save('test.jpg', 'JPEG')
+    end = time.time()
+    print(f"{end - start:.5f} sec")
+    return "finishh"
 
 
 def max_sum_sim(doc_embedding, candidate_embeddings, candidates, top_n, nr_candidates):
