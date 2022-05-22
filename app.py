25a559b55b13b6d8d3424f12cef22208ee365039
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-05-22 13:12:59 +0900

    Fix : 디버깅 모드 활성화

--- app.py
+++ app.py
@@ -7,46 +7,44 @@ from sklearn.model_selection import train_test_split
 from flask_cors import CORS
 from paint import paint
 
-paint()
-# app = Flask(__name__)
-# # 보안관련
+app = Flask(__name__)
+app.debug = True
+# 보안관련
 # CORS(app)
 # api = Api(app)
-# # app.config['DEBUG'] = True
 
 
-# @app.route('/dd')
-# def index():
-#     return 'Hello Wsldfhasdfdlskfjorld!'
+@app.route('/dd')
+def index():
+    return 'Hello'
 
 
-# @api.route('/test')
-# class testAPI(Resource):
-#     def get(self):
-#         return jsonify({"result": "연결 잘 됨 from flask"})
+@app.route('/test')
+class testAPI(Resource):
+    def get(self):
+        return jsonify({"result": "연결 잘 됨 from flask"})
 
-#     def post(self):
-#         iris = load_iris()
-#         parsed_request = request.json.get('content')
-#         result = iris.feature_names
-#         print(parsed_request)
-#         return result
-
-
-# @api.route('/paint-rec')
-# class paintAPI(Resource):
-#     def get(self):
-#         a = paint()
-#         return jsonify({"result": a})
-
-#     def post(self):
-#         iris = load_iris()
-#         parsed_request = request.json.get('content')
-#         result = iris.feature_names
-#         print(parsed_request)
-#         return result
+    def post(self):
+        iris = load_iris()
+        parsed_request = request.json.get('content')
+        result = iris.feature_names
+        print(parsed_request)
+        return result
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
-#     # app.run(host='127.0.0.1', port=8080)
+@app.route('/paint-rec')
+class paintAPI(Resource):
+    def get(self):
+        a = paint()
+        return jsonify({"result": a})
+
+    def post(self):
+        iris = load_iris()
+        parsed_request = request.json.get('content')
+        result = iris.feature_names
+        print(parsed_request)
+        return result
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
