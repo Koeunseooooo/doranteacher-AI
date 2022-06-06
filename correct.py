a15a8fd9f9b6f93216c5961f93662722dedad1f5
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-06-06 20:16:07 +0900

    Chore: py-hanspell 직접 내려받아 설치

--- correct.py
+++ correct.py
@@ -1,5 +1,12 @@
-text = '''은서네 집에 가서 햄버거를 만들었다. 
+from hanspell_mod.hanspell import spell_checker
+import pprint
+text = '''은서내 집에 가서 햄버거를 만들었다. 
 친구한테 같이 밥 먹자고 전화했는데 집에 와서 요리하자고 했다. 
 패티랑 양상추랑 토마토를 넣었다. 
 치즈도 많이 올렸다. 
 내일은 내가 잘하는 짜파게티를 해주기로 했다. 맛있겠다!'''
+
+# result = spell_checker.check(text)
+# pprint.pprint(result.as_dict())
+
+# def correct(text):
